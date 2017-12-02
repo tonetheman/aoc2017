ab2e6597754de9f530ce3b3b2ceefee5178815d5
tonetheman <tonetheman@gmail.com>
2017-12-02 10:28:24 -0600

    done with part2

--- day2/main/prog.go
+++ day2/main/prog.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-func main() {
+func part1() {
 	// open the file
 	inf, err := os.Open("part1.input")
 	if err != nil {
@@ -43,4 +43,58 @@ func main() {
 		total += diff
 	}
 	fmt.Println(total)
+}
+
+func part2() {
+	// open the file
+	inf, err := os.Open("part1.input")
+	if err != nil {
+		fmt.Println("cannot open file", err)
+	}
+	// close it later
+	defer inf.Close()
+
+	total := 0
+
+	scanner := bufio.NewScanner(inf)
+	for scanner.Scan() {
+		text := scanner.Text()
+		// split array based on space
+		// or tab
+		textNums := strings.Split(text, "\t")
+		if len(textNums) == 0 {
+			continue
+		}
+		fmt.Println(len(textNums), textNums)
+		intRow := make([]int, len(textNums))
+		for i := 0; i < len(textNums); i++ {
+			intRow[i], _ = strconv.Atoi(textNums[i])
+		}
+		fmt.Println(intRow)
+
+		for i := 0; i < len(intRow); i++ {
+			for j := 0; j < len(intRow); j++ {
+				if i != j {
+					if intRow[i]%intRow[j] == 0 {
+						fmt.Println("GOT ONE", intRow[i], intRow[j])
+						fmt.Println(intRow[i] / intRow[j])
+						fmt.Println(intRow[j] / intRow[i])
+
+						// this is a cheat one of these is 0
+						// the other is the number we care about
+						total += intRow[i] / intRow[j]
+						total += intRow[j] / intRow[i]
+					}
+				}
+			}
+		}
+		fmt.Println("-----------------")
+
+	} //
+
+	fmt.Println("total", total)
+}
+
+func main() {
+	part2()
 }
