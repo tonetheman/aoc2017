6a6aae04925f1a4dba7aadc049a5bce7360d4796
tonetheman <tonetheman@gmail.com>
2017-12-01 18:25:42 -0600

    fixed tests

--- day1/day1_test.go
+++ day1/day1_test.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"testing"
+
+	"github.com/tonetheman/aoc2017/day1/capture"
 )
 
 // TestPart1 - tests part 1 in day1
 func TestPart1(t *testing.T) {
 
 	tt := func(s string, val int) {
-		res := captcha(s)
+		res := capture.Captcha(s)
 		if res != val {
 			t.Error("sum is not ", val, res)
 		}
@@ -23,19 +25,19 @@ func TestPart1(t *testing.T) {
 
 func TestPart2(t *testing.T) {
 
-	if captcha2("1212") != 6 {
+	if capture.Captcha2("1212") != 6 {
 		t.Error("1212 is not 6")
 	}
-	if captcha2("1221") != 0 {
+	if capture.Captcha2("1221") != 0 {
 		t.Error("1221 is no 0")
 	}
-	if captcha2("123425") != 4 {
+	if capture.Captcha2("123425") != 4 {
 		t.Error("12345 is not 4")
 	}
-	if captcha2("123123") != 12 {
+	if capture.Captcha2("123123") != 12 {
 		t.Error("123123 is not 12")
 	}
-	if captcha2("12131415") != 4 {
+	if capture.Captcha2("12131415") != 4 {
 		t.Error("12131415 is not 4")
 	}
 
