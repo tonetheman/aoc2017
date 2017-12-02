4c575b79ebe22cad741208b079e4db5dfc250526
tonetheman <tonetheman@gmail.com>
2017-12-01 18:25:32 -0600

    moved cap2 into a separate file

--- day1/capture/cap.go
+++ day1/capture/cap.go
@@ -19,3 +19,18 @@ func Captcha(s string) int {
 	//fmt.Println(v1, v2)
 	return total
 }
+
+// Captcha2 does captcha for day1 part 2
+func Captcha2(s string) int {
+	var total, v1, v2 int
+
+	for i := 0; i < len(s); i++ {
+		v1, _ = strconv.Atoi(string(s[i]))
+		index := (i + len(s)/2) % len(s)
+		v2, _ = strconv.Atoi(string(s[index]))
+		if v1 == v2 {
+			total += v1
+		}
+	}
+	return total
+}
