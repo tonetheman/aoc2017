ba9b45e573fd13fa44243effb61f657e3fc696ab
Tony Colston <tonetheman@gmail.com>
2017-12-04 13:00:29 -0600

    corner and all mids done

--- day3/main/prog.go
+++ day3/main/prog.go
@@ -11,12 +11,36 @@ func bottomMid(n int) int {
 		return 1
 	}
 	rc := rightCorner(n)
-	fmt.Println("bm", rc, n)
+	//fmt.Println("bm", rc, n)
 	return rc - (n - 1)
+}
+
+func rightMid(n int) int {
+	if n == 1 {
+		return 1
+	}
+	rc := rightCorner(n)
+	return rc - (n-1)*7
+}
+
+func topMid(n int) int {
+	if n == 1 {
+		return 1
+	}
+	rc := rightCorner(n)
+	return rc - (n-1)*5
+}
+
+func leftMid(n int) int {
+	if n == 1 {
+		return 1
+	}
+	rc := rightCorner(n)
+	return rc - (n-1)*3
 }
 
 func main() {
 	for i := 1; i < 5; i++ {
-		fmt.Println(i, rightCorner(i), bottomMid(i))
+		fmt.Println(i, rightCorner(i), bottomMid(i), rightMid(i), topMid(i), leftMid(i))
 	}
 }
