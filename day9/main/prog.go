67c4add8930683af527c20fd33b094f6119e0b2c
tonetheman <tonetheman@gmail.com>
2017-12-10 12:37:59 -0600

    reverified tests with slightly simpler parser

--- day9/main/prog.go
+++ day9/main/prog.go
@@ -7,10 +7,10 @@ func parse(s string) {
 	var ignoreNextRune = false
 
 	for index, runeValue := range s {
-		fmt.Println(index, runeValue)
+		fmt.Println("NEXTCHAR:", index, runeValue, string(runeValue))
+
 		if inGarbage && ignoreNextRune {
-			fmt.Println("inGarbage and ignoreNextRune is true")
-			fmt.Println("ignoring !")
+			fmt.Println("inGarbage and ignoreNextRune is true ignoring ! and setting ignoreNext false")
 			ignoreNextRune = false
 			continue
 		}
@@ -21,6 +21,7 @@ func parse(s string) {
 				inGarbage = false
 				fmt.Println("got a > moving to a not inGarbage state", inGarbage)
 			case 33:
+				fmt.Println("got a ! setting ignoreNextRune to true")
 				ignoreNextRune = true
 			default:
 				fmt.Println("in default", runeValue, "ignoring for inGarbage")
@@ -42,13 +43,13 @@ func parse(s string) {
 }
 
 func tests() {
-	parse("<>")
+	//parse("<>")
 	//parse("<random characters>")
 	//parse("<<<<>")
 	//parse("<{!>}>")
 	//parse("<!!>")
 	//parse("<!!!>>")
-	//parse("<{o\"i!a,<{i<a>")
+	parse("<{o\"i!a,<{i<a>")
 }
 
 func main() {
