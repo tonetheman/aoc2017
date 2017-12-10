025c304f832bf2bc559a96123ea6a2f598d4770c
tonetheman <tonetheman@gmail.com>
2017-12-10 08:50:40 -0600

    wtf is wrong with golang and declared but not in use detection

--- day9/main/prog.go
+++ day9/main/prog.go
@@ -2,30 +2,26 @@ package main
 
 import "fmt"
 
-func isGarbage(s string) bool {
-	var inG bool = false
-	for c, runeValue := range s {
-		fmt.Println(c, runeValue)
-		if runeValue == 60 {
-			fmt.Println("inG set to true, in garbage")
-			inG = true
-			continue
-		}
-		if runeValue == 62 {
-			fmt.Println("inG is false out of gargage")
-			inG = false
-		}
-		if inG {
-			continue
+func parse(s string) {
+	var inGarbage = false
+	for index, runeValue := range s {
+		fmt.Println(index, runeValue)
+		switch runeValue {
+		case 60:
+			fmt.Println("got a < moving to inGarbage state")
+			inGarbage = true
+		case 62:
+			fmt.Println("got a > moving to a not inGarbage state")
+			inGarbage = false
 		}
 	}
-	return false
+	inGarbage = true
 }
 
 func tests() {
-	//isGarbage("<>")
+	parse("<>")
 	//isGarbage("<random characters>")
-	isGarbage("<<<<>")
+	//isGarbage("<<<<>")
 }
 
 func main() {
