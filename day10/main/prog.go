8848e3ff619482905688ea6997f998eb365889ce
tonetheman <tonetheman@gmail.com>
2017-12-10 15:58:30 -0600

    done with part2 of day 10

--- day10/main/prog.go
+++ day10/main/prog.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 )
 
+var skipSize = 0
+var currentPosition = 0
+
 func knothash(ll []int, ilens []int) int {
 	var LEN int = len(ll)
 
@@ -11,11 +14,11 @@ func knothash(ll []int, ilens []int) int {
 	fmt.Println("input lens", ilens)
 
 	// skip size starts at 0
-	skipSize := 0
+	//skipSize := 0
 	fmt.Println("skipSize is set to", skipSize)
 
 	// current position in the input array
-	currentPosition := 0
+	//currentPosition := 0
 	fmt.Println("current pos", currentPosition)
 
 	// current spot in the lens array
@@ -78,10 +81,12 @@ func knothash(ll []int, ilens []int) int {
 	// final 2 numbers
 	fmt.Println(ll[0], ll[1])
 	fmt.Println("answer:", ll[0]*ll[1])
+	fmt.Println("return skipSize:", skipSize)
+	fmt.Println("return currentPosition:", currentPosition)
 	return ll[0] * ll[1]
 }
 
-func main() {
+func part1() {
 	//ll := []int{0, 1, 2, 3, 4}
 	//ilens := []int{3, 4, 1, 5}
 	ll := make([]int, 256)
@@ -91,4 +96,73 @@ func main() {
 	fmt.Println(ll)
 	ilens := []int{129, 154, 49, 198, 200, 133, 97, 254, 41, 6, 2, 1, 255, 0, 191, 108}
 	knothash(ll, ilens)
+}
+
+func compute_spare_hash(a []int) int {
+	total := 0
+	for i := 0; i < 16; i++ {
+		total ^= a[i]
+	}
+	return total
+}
+
+func part2(s string) {
+	input_list := make([]int, 0)
+	for _, runeValue := range s {
+		input_list = append(input_list, int(runeValue))
+	}
+	input_list = append(input_list, 17)
+	input_list = append(input_list, 31)
+	input_list = append(input_list, 73)
+	input_list = append(input_list, 47)
+	input_list = append(input_list, 23)
+	fmt.Println("s", s)
+	fmt.Println("input_list", input_list)
+
+	ll := make([]int, 256)
+	for i := 0; i < 256; i++ {
+		ll[i] = i
+	}
+
+	for i := 0; i < 64; i++ {
+		knothash(ll, input_list)
+	}
+
+	fmt.Println("END OF 64 ROUNDS")
+	fmt.Println(ll)
+	res := sp(ll)
+	fmt.Println("res is", res)
+	for _, val := range res {
+		fmt.Printf("%2x", val)
+	}
+	fmt.Println()
+}
+
+func sp(a []int) []int {
+	//a := make([]int, 256)
+	//for i := 0; i < 256; i++ {
+	//	a[i] = rand.Intn(255)
+	//}
+	//fmt.Println(a)
+	res := make([]int, 16)
+	for i := 0; i < 16; i++ {
+		fmt.Println(i*16, (i+1)*16, a[i*16:(i+1)*16])
+		res[i] = compute_spare_hash(a[i*16 : (i+1)*16])
+	}
+	fmt.Println(res)
+	return res
+}
+func main() {
+	part2("129,154,49,198,200,133,97,254,41,6,2,1,255,0,191,108")
+	//a := []int{65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22}
+	//fmt.Println(compute_spare_hash(a))
+	//a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	//fmt.Println(a[0:3])
+	//fmt.Println(a[3 : 3+3])
+	//a := make([]int, 256)
+	//for i := 0; i < 256; i++ {
+	//	a[i] = rand.Intn(255)
+	//}
+	//fmt.Println(a)
+	//sp(a)
 }
