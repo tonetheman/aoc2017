bc356f42d6cb68e3abd33bfdfb89224d0a84685e
tonetheman <tonetheman@gmail.com>
2017-12-14 14:59:13 -0600

    saving work not sure if timing is correct yet

--- day13/main/prog.go
+++ day13/main/prog.go
@@ -8,6 +8,9 @@ import (
 	"strings"
 )
 
+const SCANNER_DOWN = 9
+const SCANNER_UP = 8
+
 type layer struct {
 	layer_num int
 	depth     int
@@ -42,9 +45,107 @@ func findMaxScanner(layers []layer) int {
 	return maxValue
 }
 
+func insert_scanners(ll [][]int) {
+	for i := 0; i < len(ll); i++ {
+		target := ll[i]
+		if len(target) > 0 {
+			target[0] = SCANNER_DOWN
+		}
+	}
+}
+
+func findScannerIndex(a []int) int {
+	for i := 0; i < len(a); i++ {
+		if a[i] == SCANNER_DOWN || a[i] == SCANNER_UP {
+			return i
+		}
+	}
+	return -1
+}
+
+func moveAllScanners(ll [][]int) {
+	for i := 0; i < len(ll); i++ {
+		if len(ll[i]) > 0 {
+			currentIndex := findScannerIndex(ll[i])
+			if ll[i][currentIndex] == SCANNER_DOWN {
+				newIndex := currentIndex + 1
+				if newIndex == len(ll[i]) {
+					// change to scanner up
+					ll[i][currentIndex] = 0
+					// not sure about this yet -2
+					ll[i][newIndex-2] = SCANNER_UP
+				} else {
+					ll[i][currentIndex] = 0
+					ll[i][newIndex] = SCANNER_DOWN
+				}
+			} else {
+				// scanner up case
+				newIndex := currentIndex - 1
+				if newIndex == -1 {
+					// change scanner to up
+				} else {
+					// just move it
+					ll[i][currentIndex] = 0
+					ll[i][newIndex] = SCANNER_UP
+				}
+			}
+		}
+	}
+}
+
+func sim(ll [][]int) {
+	fmt.Println("sim started...")
+	insert_scanners(ll)
+	fmt.Println("inserting scanners (9)...")
+	fmt.Println(ll)
+	fmt.Println("starting loop...")
+	count := 0
+	playerPosition := -1 //player starts off the grid
+	cost := 0
+	for {
+		fmt.Println("SECOND", count)
+		// move player and see if he is caught
+		playerPosition++
+		fmt.Println("player position is", playerPosition)
+		fmt.Println(ll)
+
+		// if the layer has anything in it
+		if len(ll[playerPosition]) > 0 {
+
+			// check if the layer has a scanner
+			if ll[playerPosition][0] == SCANNER_DOWN ||
+				ll[playerPosition][0] == SCANNER_UP {
+				// if caught you incur depth * range
+				cost += (playerPosition) * len(ll[playerPosition])
+				fmt.Println("cost added", cost)
+
+			}
+		}
+
+		// now move all the scanners
+		fmt.Println("calling move all scanners...")
+		moveAllScanners(ll)
+		fmt.Println(ll)
+		fmt.Println("---------------")
+		count++
+		if count == 3 {
+			break
+		}
+	}
+	fmt.Println("end sim")
+}
+
 func main() {
 	layers := readFile("test.input")
 	fmt.Println(layers)
 	maxLayerNum := findMaxScanner(layers)
 	fmt.Println("max layer num", maxLayerNum)
+	ll := make([][]int, maxLayerNum+1)
+	for i := 0; i < len(layers); i++ {
+		target := layers[i]
+		ll[target.layer_num] = make([]int, target.depth)
+	}
+	fmt.Println(ll)
+
+	sim(ll)
 }
