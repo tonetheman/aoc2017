9f81fed1d1670d5293b680a81fec704349a53113
Tony Colston <tonetheman@gmail.com>
2017-12-19 13:13:41 -0600

    works much faster... prob could be faster done with part2

--- day13/math_aoc13.py
+++ day13/math_aoc13.py
@@ -56,20 +56,52 @@ def calc_hit(period,step,delay):
     m = (period-1)*2
     return (step)%m==0
 
-data = read_input_file("test.input")
-#data = read_input_file("part1.input")
-cost = 0
-delay = 0
-print("delay is",delay)
-for i in range(6+1):
-#for i in range(88+1):
-    d = find_layer(i,data)
-    if d is not None:
-        if calc_hit(d[1],i,delay):
-            cost = cost + (i*d[1])
-        print(i,d,calc_hit(d[1],i,delay))
-    else:
-        print(i,d)
-print("final cost",cost)
+def part2not():
+	data = read_input_file("test.input")
+	#data = read_input_file("part1.input")
+	cost = 0
+	delay = 0
+	print("delay is",delay)
+	for i in range(6+1):
+	#for i in range(88+1):
+	    d = find_layer(i,data)
+	    if d is not None:
+		if calc_hit(d[1],i,delay):
+		    cost = cost + (i*d[1])
+		print(i,d,calc_hit(d[1],i,delay))
+	    else:
+		print(i,d)
+	print("final cost",cost)
+
+def tonypart2():
+	def calc(period,step):
+		m = (period-1)*2
+		return step%m == 0
+
+	#data = read_input_file("test.input")
+	data = read_input_file("part1.input")
+
+	# i is the amount of delay
+	for i in range(3000000,4000000):
+		if i%1000==0:
+			print(i,"*")
+		cost = False
+		# print("delay is",i,"start without cost",cost)
+		
+		# j here is taking the step across the scanners
+		#for j in range(6+1):
+		for j in range(88+1):
+			d = find_layer(j,data)
+			if d is not None:
+				# print "layer",d, calc(d[1], i+j)
+				if calc(d[1],i+j):
+					cost = True
+		# print("delay end",i,"cost is",cost)
+
+		if cost == False:
+			print "FOUND ONE WITHOUT COST",i
+			return			
+
+tonypart2()
 
 
